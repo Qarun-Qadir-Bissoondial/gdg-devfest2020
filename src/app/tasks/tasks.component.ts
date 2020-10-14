62e8040c28fc4e776bd6d078b66157d516781a8f
Qarun Qadir Bissoondial <qarun80@hotmail.com>
2020-10-13 21:54:50 -0400

    first commit

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+export interface Task {
+  title: string;
+  description?: string;
+  completed: boolean;
+}
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.scss'],
+})
+export class TasksComponent {
+
+  tasks: Task[] = [
+    { title: 'Go Grocery Shopping', description: 'Eggs, cheese, butter, milk', completed: false },
+    { title: 'Walk dog', description: '3 laps around walking track', completed: false },
+    { title: 'Write article', description: 'Linked List Data Structure', completed: false },
+  ]
+  counter: number;
+
+  constructor() {
+    this.counter = this.tasks.length;
+  }
+
+  toggleStatus(task: Task) {
+    task.completed = !task.completed
+  } 
+
+  deleteItem(id: number) {
+    this.tasks.splice(id, 1);
+  }
+
+  addTask() {
+    this.tasks.push({
+      title: `Dummy task ${this.counter + 1}`,
+      description: 'Lorem Ipsum yada yada yada',
+      completed: false
+    });
+    this.counter++;
+  }
+
+}
